e2cb45441717b5d106327a1b4e74ccba18f0a8e3
Abdullah Khan <khanabdullahj@gmail.com>
2016-11-11 22:30:54 -0500

    Reimplement functions for matrix inversion.

--- s3.cpp
+++ s3.cpp
@@ -17,16 +17,40 @@ using namespace ComputerVisionProjects;
 using Matrix3 = array<array<double, 3>, 3>;
 
 // Matrix operations
-void InverseDet(Matrix3 &a, double det);
+
+double GetDeterminant(Matrix3 A)
+{
+	double a = (A[0][0] * A[1][1] * A[2][2]) + (A[0][1] * A[1][2] * A[2][0]) + (A[0][2] * A[1][0] * A[2][1]);
+	double b = (A[2][0] * A[1][1] * A[0][2]) + (A[2][1] * A[1][2] * A[0][0]) + (A[2][2] * A[1][0] * A[0][1]);
+	return a-b;
+}
 
 /*
- * The following functions were taken from
- * 		https://www.cs.rochester.edu/~brown/Crypto/assts/projects/adj.html
- * and edited to work with types Matrix3 and changed to be more like Modern C++.
- */
-double Determinant(Matrix3 a, int n);
-Matrix3 CoFactor(Matrix3 a, int n);
-void Transpose(Matrix3 &a, int n);
+Implemented from the information given here:
+http://www.cg.info.hiroshima-cu.ac.jp/~miyazaki/knowledge/teche23.html
+Much easier than handling co-factors.
+*/
+Matrix3 GetInverse(Matrix3 A, double determinant)
+{
+	Matrix3 B = {};
+	double invdet = 1/determinant;
+
+	// First row
+	B[0][0] = (A[1][1] * A[2][2] - A[1][2] * A[2][1]) * invdet;
+	B[0][1] = (A[0][3] * A[2][1] - A[0][1] * A[2][2]) * invdet;
+	B[0][2] = (A[0][1] * A[1][2] - A[0][2] * A[1][1]) * invdet;
+
+	// Second row
+	B[1][0] = (A[1][2] * A[2][0] - A[1][0] * A[2][2]) * invdet;
+	B[1][1] = (A[0][0] * A[2][2] - A[0][2] * A[2][0]) * invdet;
+	B[1][2] = (A[0][2] * A[1][0] - A[0][0] * A[1][2]) * invdet;
+
+	// Third row
+	B[2][0] = (A[1][0] * A[2][1] - A[1][1] * A[2][0]) * invdet;
+	B[2][1] = (A[0][1] * A[2][0] - A[0][0] * A[2][1]) * invdet;
+	B[2][2] = (A[0][0] * A[1][1] - A[0][1] * A[1][0]) * invdet;
+	return B;
+}
 
 int main(int argc, char ** argv)
 {
@@ -88,31 +112,34 @@ int main(int argc, char ** argv)
 		in.close();
 	} // Read directions file into matrix.
 	
-	double det = Determinant(matrix,3);
-	Matrix3 inverse = CoFactor(matrix, 3);
-	Transpose(inverse, 3);
-	InverseDet(inverse, det);
+	// Find determinant and invert the matrix.
+	double det = GetDeterminant(matrix);
+	Matrix3 inverse = GetInverse(matrix, det);
 
+	// Loop through image, incrementing by step.
 	{
 		for (unsigned int i = 0; i < object1.num_rows(); i+=step)
 		{
 			for (unsigned int j = 0; j < object1.num_columns(); j+=step)
 			{
+				// Get current pixel brightness values from each image.
 				double b1 = object1.GetPixel(i,j);
 				double b2 = object2.GetPixel(i,j);
 				double b3 = object3.GetPixel(i,j);
 				if (b1 > threshold && b2 > threshold && b3 > threshold)
 				{
+					// Make black ring around current pixel.
 					object1.SetPixel(i-1,j,0);
 					object1.SetPixel(i,j-1,0);
 					object1.SetPixel(i,j+1,0);
 					object1.SetPixel(i+1,j,0);
-					
+
+					// Calculate the normal.					
 					double x = inverse[0][0] * b1 + inverse[0][1] * b2 + inverse[0][2] * b3;
 					double y = inverse[1][0] * b1 + inverse[1][1] * b2 + inverse[1][2] * b3;
 					double z = inverse[2][0] * b1 + inverse[2][1] * b2 + inverse[2][2] * b3;
-					double mag = sqrt( x*x + y*y + z*z );
-					x = x/mag; y = y/mag; z = z/mag;
+					double mag = sqrt( x*x + y*y + z*z ); // Get the magnitude.
+					x = x/mag; y = y/mag; z = z/mag; // Scale and draw our line.
 					DrawLine(i, j, (int)(i + 10 * x), (int)(j + 10 * y), 255, &object1);
 				}
 			}
@@ -126,90 +153,4 @@ int main(int argc, char ** argv)
 		return 0;
 	}
 	return 0;
-}
-
-double Determinant(Matrix3 a, int n)
-{
-	int i,j,j1,j2;
-	double det = 0;
-	Matrix3 m = {};
-
-	if (n < 1) { return 0; }
-	else if (n == 1) { det = a[0][0]; }
-	else if (n == 2) { det = a[0][0] * a[1][1] - a[1][0] * a[0][1]; }
-	else
-	{
-		det = 0;
-		for (j1=0;j1<n;j1++) {
-			for (i=1;i<n;i++) {
-				j2 = 0;
-				for (j=0;j<n;j++) {
-					if (j == j1) continue;
-					m[i-1][j2] = a[i][j];
-					j2++;
-				}
-			}
-			det += pow(-1.0,j1+2.0) * a[0][j1] * Determinant(m,n-1);
-		}
-	}
-	return(det);
-}
-
-Matrix3 CoFactor(Matrix3 a, int n)
-{
-	int i,j,ii,jj,i1,j1;
-	double det;
-	Matrix3 b = {};
-	Matrix3 c = {};
-
-	for (j=0;j<n;j++) {
-		for (i=0;i<n;i++) {
-
-		/* Form the adjoint a_ij */
-			i1 = 0;
-			for (ii=0;ii<n;ii++) {
-				if (ii == i)
-					continue;
-				j1 = 0;
-				for (jj=0;jj<n;jj++) {
-					if (jj == j)
-						continue;
-					c[i1][j1] = a[ii][jj];
-					j1++;
-				}
-				i1++;
-			}
-
-		/* Calculate the determinate */
-			det = Determinant(c,n-1);
-
-		/* Fill in the elements of the cofactor */
-			b[i][j] = pow(-1.0,i+j+2.0) * det;
-		}
-	}
-	return b;
-}
-
-void Transpose(Matrix3 &a, int n)
-{
-	double tmp;
-
-	for (int i = 1; i < n; i++) {
-		for (int j = 0; j < i; j++) {
-			tmp = a[i][j];
-			a[i][j] = a[j][i];
-			a[j][i] = tmp;
-		}
-	}
-}
-
-void InverseDet(Matrix3 &a, double det)
-{
-	for (int i=0; i < 3; i++)
-	{
-		for (int j=0; j < 3; j++)
-		{
-			a[i][j] = (1/det) * a[i][j];
-		}
-	}
 }
